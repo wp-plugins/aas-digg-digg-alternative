31ce78259c95210f6394c3f14c559faa4f1a84aa
Brandon_Orndorff <Brandon_Orndorff@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-24 16:37:38 +0000

    Added the ability to show on posts and/or pages along with floating location settings for each.
    git-svn-id: https://plugins.svn.wordpress.org/aas-digg-digg-alternative/trunk@616921 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- floating-social-admin.php
+++ floating-social-admin.php
@@ -2,12 +2,15 @@
 if(isset($_POST['mokis_social_data_left'])){
 	update_option('mokis_social_data_left', $_POST['mokis_social_data_left']);
 	update_option('mokis_social_data_top', $_POST['mokis_social_data_top']);
+	update_option('mokis_social_data_left_pages', $_POST['mokis_social_data_left_pages']);
+	update_option('mokis_social_data_top_pages', $_POST['mokis_social_data_top_pages']);
 	update_option('mokis_show_googleplus', $_POST['mokis_show_googleplus']);
 	update_option('mokis_show_digg', $_POST['mokis_show_digg']);
-	
 	update_option('mokis_show_twitter', $_POST['mokis_show_twitter']);
 	update_option('mokis_show_facebook', $_POST['mokis_show_facebook']);
 	update_option('mokis_show_stumble', $_POST['mokis_show_stumble']);
+	update_option('mokis_social_data_posts', $_POST['mokis_social_data_posts']);
+	update_option('mokis_social_data_pages', $_POST['mokis_social_data_pages']);
 	update_option('mokis_social_data_credit', $_POST['mokis_social_data_credit']);
 	
 	$mokisfloatsuccess = "Settings Updated Successfully!";
@@ -31,15 +34,24 @@ if(isset($_POST['mokis_social_data_left'])){
 	<tbody>
 	   <tr>
 		 <td style="padding:25px;font-family:Verdana, Geneva, sans-serif;color:#666;">
-		 	<h2>Page Location Settings:</h2>
+		 	<h2>Post/Page Location Settings:</h2>
+		 	<h3 style="padding:0;margin:0;">Posts - location on posts</h3>
              <label for="msf_float_left">
                  <p><input type="text" name="mokis_social_data_left" value="<?php echo get_option('mokis_social_data_left'); ?>" style="width:40px;" /> :<b>Left Margin (pixels)</b> - How far left from your content you want the bar to float.</p>
              </label>
              <label for="mokis_social_data_top">
                  <p><input type="text" name="mokis_social_data_top" value="<?php echo get_option('mokis_social_data_top'); ?>" style="width:40px;" /> :<b>Top Margin (pixels)</b> - How far up from your content you want the bar to float.</p>
              </label>
+             <br />
+             <h3 style="padding:0;margin:0;">Pages - location on pages</h3>
+             <label for="msf_float_left_pages">
+                 <p><input type="text" name="mokis_social_data_left_pages" value="<?php echo get_option('mokis_social_data_left_pages'); ?>" style="width:40px;" /> :<b>Left Margin (pixels)</b> - How far left from your content you want the bar to float.</p>
+             </label>
+             <label for="mokis_social_data_top_pages">
+                 <p><input type="text" name="mokis_social_data_top_pages" value="<?php echo get_option('mokis_social_data_top_pages'); ?>" style="width:40px;" /> :<b>Top Margin (pixels)</b> - How far up from your content you want the bar to float.</p>
+             </label>
              
-             <br /><br />
+             <hr />
              
              <?php
              if(get_option('mokis_show_googleplus') == 1) {$gpluschecked = checked;}
@@ -47,6 +59,8 @@ if(isset($_POST['mokis_social_data_left'])){
 			 if(get_option('mokis_show_twitter') == 1) {$twitterchecked = checked;}
 			 if(get_option('mokis_show_facebook') == 1) {$facebookchecked = checked;}
 			 if(get_option('mokis_show_stumble') == 1) {$stumblechecked = checked;}
+			 if(get_option('mokis_social_data_posts') == 1) {$postsshowchecked = checked;}
+			 if(get_option('mokis_social_data_pages') == 1) {$pagesshowchecked = checked;}
 			 if(get_option('mokis_social_data_credit') == 1) {$creditchecked = checked;}
              ?>
              
@@ -66,8 +80,19 @@ if(isset($_POST['mokis_social_data_left'])){
              <label for="mokis_show_stumble">
                  <p><input type="checkbox" name="mokis_show_stumble" value="1" <?php echo $stumblechecked; ?> /> :<b>Stumble</b> - Check this box to show Stumble on your Floating bar.</p>
              </label>
+             More Social Networks will be added shortly (we are going through extensive testing and implementing lazy loading first)
              
-             <br /><br />
+             <hr />
+             
+             <h2>Where to Show the Floating Bar:</h2>
+             <label for="mokis_social_data_posts">
+                 <p><input type="checkbox" name="mokis_social_data_posts" value="1" <?php echo $postsshowchecked; ?> /> :<b>Posts</b> - Check this box to show the floating social bar on posts.</p>
+             </label>
+             <label for="mokis_social_data_pages">
+                 <p><input type="checkbox" name="mokis_social_data_pages" value="1" <?php echo $pagesshowchecked; ?> /> :<b>Pages</b> - Check this box to show the floating social bar on pages.</p>
+             </label>
+             
+             <hr />
              
              <h2>Other Settings:</h2>
              <label for="mokis_social_data_credit">
--- floating-social-bar.php
+++ floating-social-bar.php
@@ -3,7 +3,7 @@
 Plugin Name: AA's Digg Digg Alternative
 Plugin URI: http://www.android-advice.com/2012/faster-seo-friendly-digg-digg-alternative-wordpress-plugin/
 Description: Floating social bar for those that don't want to use the content heavy and slow Digg Digg bar.  This bar only has the code required to create the floating bar with sharing to increase page speed and less code.
-Version: 1.0.1
+Version: 1.1
 Author: Brandon Orndorff
 Author URI: http://www.android-advice.com
 License: GPL2
@@ -18,14 +18,23 @@ class Mokis_Digg_Alt_Controller {
 		register_uninstall_hook( __FILE__, array( __CLASS__, 'uninstall' ) );
 		add_option("mokis_social_data_left", '-90', '', 'yes');
 		add_option("mokis_social_data_top", '-50', '', 'yes');
+		add_option("mokis_social_data_left_pages", '-90', '', 'yes');
+		add_option("mokis_social_data_top_pages", '-50', '', 'yes');
 		add_option("mokis_show_googleplus", '1', '', 'yes');
 		add_option("mokis_show_digg", '1', '', 'yes');
 		add_option("mokis_show_twitter", '1', '', 'yes');
 		add_option("mokis_show_facebook", '1', '', 'yes');
 		add_option("mokis_show_stumble", '1', '', 'yes');
+		add_option("mokis_social_data_posts", '1', '', 'yes');
+		add_option("mokis_social_data_pages", '', '', 'yes');
 	}
 	function activate() {
 		add_option("mokis_social_data_credit", '1', '', 'yes');
+		update_option('mokis_social_data_credit', '1');
+		add_option("mokis_social_data_posts", '1', '', 'yes');
+		add_option("mokis_social_data_pages", '', '', 'yes');
+		add_option("mokis_social_data_left_pages", '-90', '', 'yes');
+		add_option("mokis_social_data_top_pages", '-50', '', 'yes');
 	}
 
 	function deactivate() {
@@ -40,12 +49,18 @@ class Mokis_Digg_Alt_Controller {
 		delete_option('mokis_show_twitter');
 		delete_option('mokis_show_facebook');
 		delete_option('mokis_show_stumble');
+		delete_option('mokis_social_data_posts');
+		delete_option('mokis_social_data_pages');
+		delete_option('mokis_social_data_left_pages');
+		delete_option('mokis_social_data_top_pages');
 	}
 }
 
 function moki_add_social_content($content) {
+		
 	global $wpdb;
-	if ( is_single() ){
+	//Start if is Post
+	if ( is_single() && get_option('mokis_social_data_posts') == 1 ){
 	?>
 <div style="-moz-border-radius: 10px; border-radius: 10px; border:1px solid #999999; position: fixed; z-index:99999; width: 70px; background-color:#FFFFFF; display:block; margin-left: <?php echo get_option('mokis_social_data_left'); ?>px; margin-top: <?php echo get_option('mokis_social_data_top'); ?>px;">
 <div style="width:68px;margin: auto; padding:0 0 5px 0; text-align:center;">
@@ -74,6 +89,37 @@ function moki_add_social_content($content) {
 </div>
 <?php
 } //end if is_single
+
+//Start if is Page
+	if ( is_page() && get_option('mokis_social_data_pages') == 1 ){
+	?>
+<div style="-moz-border-radius: 10px; border-radius: 10px; border:1px solid #999999; position: fixed; z-index:99999; width: 70px; background-color:#FFFFFF; display:block; margin-left: <?php echo get_option('mokis_social_data_left_pages'); ?>px; margin-top: <?php echo get_option('mokis_social_data_top_pages'); ?>px;">
+<div style="width:68px;margin: auto; padding:0 0 5px 0; text-align:center;">
+<?php if(get_option('mokis_show_googleplus') == 1){ ?><span style="padding-top:5px; display:block;"><script type='text/javascript' src='https://apis.google.com/js/plusone.js'></script><g:plusone size='tall'></g:plusone></span><?php } ?>
+<?php if(get_option('mokis_show_digg') == 1){ ?><span style="padding-top:5px; display:block;"><script type='text/javascript'>(function() {var s = document.createElement('SCRIPT'), s1 = document.getElementsByTagName('SCRIPT')[0];s.type = 'text/javascript';s.async = true;s.src = 'http://widgets.digg.com/buttons.js';s1.parentNode.insertBefore(s, s1);})();</script> <a class='DiggThisButton DiggMedium' href='http://digg.com/submit'></a></span><?php } ?>
+<?php if(get_option('mokis_show_twitter') == 1){ ?><span style="padding-top:5px; display:block;"><a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical"></a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></span><?php } ?>
+<?php if(get_option('mokis_show_facebook') == 1){ ?><span style="padding-top:5px; display:block;"><div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-send="false" data-layout="box_count" data-width="55" data-show-faces="false"></div></span><?php } ?>
+<?php if(get_option('mokis_show_stumble') == 1){ ?><span style="padding-top:5px; display:block;"><su:badge layout="5"></su:badge>
+<script type="text/javascript">
+  (function() {
+    var li = document.createElement('script'); li.type = 'text/javascript'; li.async = true;
+    li.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + '//platform.stumbleupon.com/1/widgets.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(li, s);
+  })();
+</script></span><?php } ?>
+<?php if(get_option('mokis_social_data_credit') == 1){ ?><span style="padding-top:5px; display:block;"><a href="http://www.android-advice.com" target="_blank" title="Android News, Tutorials, how to's, applications'" style="font-size:9px;">Powered By:<br />Android Advice</a></span><?php } ?>
+</div>
+</div>
+<?php
+} //end if is_page
 return $content;
 
 } //end function
